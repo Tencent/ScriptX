cd2915b543b598fa4a976745dc7cb19e94490846
taylorcyang <taylorcyang@tencent.com>
2021-04-07 17:59:15 +0800

    change log

--- src/version.h
+++ src/version.h
@@ -17,9 +17,9 @@
 
 // ScriptX version config files
 // auto generated from the file VERSION
-#define SCRIPTX_VERSION_STRING "3.0.0"
+#define SCRIPTX_VERSION_STRING "3.1.0"
 #define SCRIPTX_VERSION_MAJOR 3
-#define SCRIPTX_VERSION_MINOR 0
+#define SCRIPTX_VERSION_MINOR 1
 #define SCRIPTX_VERSION_PATCH 0
 
 constexpr const char* kVersionString = SCRIPTX_VERSION_STRING;
