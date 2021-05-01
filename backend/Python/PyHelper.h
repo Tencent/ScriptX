eebd4c24ee9e9b4319b436219dfd6bc014ef9b12
landerlyoung <landerlyoung@gmail.com>
2021-05-01 19:46:35 +0800

    include

---
+++ backend/Python/PyHelper.h
@@ -0,0 +1,31 @@
+/*
+ * Tencent is pleased to support the open source community by making ScriptX available.
+ * Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "../../src/foundation.h"
+
+// docs: https://docs.python.org/3/c-api/index.html
+
+SCRIPTX_BEGIN_INCLUDE_LIBRARY
+#ifndef PY_SSIZE_T_CLEAN
+#define PY_SSIZE_T_CLEAN
+#endif
+#include <Python.h>
+SCRIPTX_END_INCLUDE_LIBRARY
+
+namespace script::py_backend {}
